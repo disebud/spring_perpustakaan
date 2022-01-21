bf95d7fcb49631b0e2b052f18e17edd95fee1719
disebud <disebud@gmail.com>
2022-01-21 23:29:53 +0700

    NEXT 1

---
+++ src/main/java/com/sinaukoding/perpustakaan/controller/BaseController.java
@@ -0,0 +1,8 @@
+package com.sinaukoding.perpustakaan.controller;
+
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+@PreAuthorize("isFullyAuthenticated()")
+public abstract class BaseController {
+}
