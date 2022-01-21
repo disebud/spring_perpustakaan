b36b31db74043060cc76c8476fd618096219eb20
disebud <disebud@gmail.com>
2022-01-22 00:49:46 +0700

    NEXT 1

--- src/main/java/com/sinaukoding/perpustakaan/config/WebSecurityConfiguration.java
+++ src/main/java/com/sinaukoding/perpustakaan/config/WebSecurityConfiguration.java
@@ -33,6 +33,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http.csrf().disable()
                 .authorizeRequests().antMatchers("/books").permitAll()
                 .anyRequest().authenticated().and()
